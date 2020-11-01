55d4f179316f087e8e717f2e673b081b53e30687
James Constable <jwdconstable@gmail.com>
2020-11-02 01:46:44 +1100

    Fix scaling and annulus sector bugs, and associated refactoring

--- svg-generation/src/HoneyTime.hs
+++ svg-generation/src/HoneyTime.hs
@@ -8,15 +8,25 @@ import qualified Data.Text as T
 tshow :: Show a => a -> T.Text
 tshow = T.pack . show
 
-range :: (Integral a, Num b) => a -> [b]
-range n = map fromIntegral [0..(n-1)]
+apothem :: (Integral a, RealFloat b) => a -> b -> b
+apothem n radius = radius * cos (pi / fromIntegral n)
+
+polygonAngle :: (Integral a, RealFloat b) => a -> Bool -> a -> b
+polygonAngle n vertexAtTop i =
+  let
+    sides  = fromIntegral n
+    offset = if vertexAtTop then 0 else pi/sides
+  in (fromIntegral i) * 2*pi/sides - pi/2 + offset
+
+hexagonAngle :: (Integral a, RealFloat b) => a -> b
+hexagonAngle = polygonAngle 6 False
 
 point :: RealFloat a => a -> a -> T.Text
-point x y = T.concat [toText x, ",", toText y, " "]
+point x y = T.concat [toText x, " ", toText y, " "]
 
 arc :: RealFloat a => a -> a -> a -> T.Text
 arc targetAngle radius rotation = T.intercalate " " [
-  "A",                                -- absolute-valued arc command
+  "A",                                -- command for arc with absolute values
   toText radius,                      -- x-radius
   toText radius,                      -- y-radius
   toText (abs rotation),              -- x-axis rotation
@@ -29,34 +39,25 @@ arc targetAngle radius rotation = T.intercalate " " [
 polarTranslate :: RealFloat a => a -> a -> T.Text
 polarTranslate r theta = translate (r * cos theta) (r * sin theta)
 
-polygonAngle :: (Integral a, RealFloat b) => a -> a -> b
-polygonAngle n i = (fromIntegral i - 1) * 2 * pi / (fromIntegral n)
-
-hexagonAngle :: (Integral a, RealFloat b) => a -> b
-hexagonAngle = polygonAngle 6
-
-apothem :: (Integral a, RealFloat b) => a -> b -> b
-apothem n radius = radius * cos (pi / fromIntegral n)
-
-polygon :: (Integral a, RealFloat b) => a -> b -> b -> b -> b -> Element
-polygon n x y radius rotation =
+polygon :: (Integral a, RealFloat b) => a -> b -> b -> b -> Bool -> Element
+polygon n centreX centreY radius vertexAtTop =
   let
+    element p = polygon_ [Points_ <<- p]
     calcVertex i =
-      let theta = pi/2 - 2*pi*i/(fromIntegral n) - rotation
-      in point (x + radius * cos theta) (y - radius * sin theta)
-    points = map calcVertex (range n)
-  in polygon_ [Points_ <<- mconcat points]
+      let theta = polygonAngle n vertexAtTop i
+      in point (centreX + radius * cos theta) (centreY + radius * sin theta)
+  in element $ mconcat $ map calcVertex [0..(n-1)]
 
-hexagon :: RealFloat a => a -> a -> a -> a -> Element
+hexagon :: RealFloat a => a -> a -> a -> Bool -> Element
 hexagon = polygon 6
 
-annulusSector :: (Integral a, RealFloat b, Integral c, Show c)
-              => a -> b -> b -> c -> Element
+annulusSector :: (Integral a, Show a, RealFloat b)
+              => a -> b -> b -> a -> Element
 annulusSector n outerRadius innerRadius i =
   let
-    theta = 2 * pi / (fromIntegral n)
-    start = -pi/2 + theta * (fromIntegral i)
-    end   = -pi/2 + theta * (fromIntegral i + 1)
+    theta = 2 * pi / fromIntegral n
+    start = polygonAngle n True i
+    end   = polygonAngle n True (i+1)
   in path_ [
     Class_ <<- ("sector" <> tshow i),
     D_ <<- (
@@ -100,16 +101,20 @@ outerClockDial hexRadius =
 clockDial :: Element
 clockDial =
   let
-    tileId = "#hex_tile"
-    tileRadius = 15
-    centreTranslate = translate (12 * tileRadius) (12 * tileRadius)
-  in
-    defs_ [] (with (hexagon 0 0 15 0) [Id_ <<- T.tail tileId])
-    <> with (innerClockDial tileRadius tileId) [Transform_ <<- centreTranslate]
-    <> with (outerClockDial tileRadius) [Transform_ <<- centreTranslate]
+    tileId      = "#hex-tile"
+    tileRadius  = 10
+    imageWidth  = 250
+    imageHeight = 250
+  in g_ [
+    Class_     <<- "clock-dial",
+    Transform_ <<- translate (imageWidth / 2) (imageHeight / 2)]
+    (
+    defs_ [] (with (hexagon 0 0 tileRadius True) [Id_ <<- T.tail tileId])
+    <> innerClockDial tileRadius tileId
+    <> outerClockDial tileRadius)
 
 svg :: Element -> Element
 svg content =
   doctype
   <> with (svg11_ content) [
-    Version_ <<- "1.1", Width_ <<- "500", Height_ <<- "500"]
+    Version_ <<- "1.1", Width_ <<- "250", Height_ <<- "250"]
